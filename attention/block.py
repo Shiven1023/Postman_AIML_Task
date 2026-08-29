a78feea3c34f5590c8bbfc173a2cd5cb9ad62288
Shiven1023 <shiveng1023@gmail.com>
2026-08-29 22:14:50 +0530

    block sparse added

--- attention/block.py
+++ attention/block.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 
 """
 q , k ,v --> (B,H ,N , d )9after permutation) (N--> seq_len)
-qi --> (B ,H ,1 ,d) , ki, vi --> (B ,H , window_size , d)
-score(i) = (B, H , 1 , window_size) (ith tokens scores) --> softmax this
-out(i) = (B, H , 1 , d) 
+qi --> (B ,H ,block_size ,d) , ki, vi --> (B ,H , (num_blocks+1)*block_size , d) (in the middle somewhere)
+score , attention(i) --> (B, H , block_size , (num_blocks+1)*block_size)
+causal mask needed to prevent score computing for the earlier values in a block with further ones in same block
 
 """
 class SlidingWindowAttention(nn.Module):
-    def __init__(self, d, n_heads, window_size):
+    def __init__(self, d, n_heads, block_size,num_blocks):
         super().__init__()
         self.d = d
         self.n_heads = n_heads
         self.head_dim = d // n_heads
-        slef.window_size = window_size
+        self.num_blocks = num_blocks #how many blocks excluding current to query with
+        self.block_size = block_size
+
         assert d % n_heads == 0 
 
         self.in_proj = nn.Linear(d, 3 * d)
@@ -36,14 +38,19 @@ class SlidingWindowAttention(nn.Module):
         v = v.permute(0, 2, 1, 3)
         
         outputs = []
-        for i int range(N):
-            start = max(0, i - self.window_size + 1)
-            qi = q[: , : ,i: i+1,:]
-            ki = k[:, :, start:i+1, :]
-            vi = v[:, :, start:i+1, :]
+        for i in range(0,N, self.block_size):
+            start = max(0, i - self.num_blocks*self.block_size)
+            end = min(i + self.block_size , N)
+            qi = q[: , : ,i: end,:]
+            ki = k[:, :, start:end, :]
+            vi = v[:, :, start:end, :]
             scores = qi @ ki.transpose(-2, -1)
 
             scores = scores / (self.head_dim ** 0.5)
+            q_indices = torch.arange(i, end, device=x.device)[:, None] #(block,size , 1) size column vector
+            k_indices = torch.arange(start, end, device=x.device)[None, :] #(1, block_size*(num_blocks+1)) size row vector 
+            allowed = k_indices <= q_indices
+            scores = scores.masked_fill(~allowed, float("-inf"))
 
             attention = torch.softmax(scores, dim=-1)
             out_i = attention @ vi
